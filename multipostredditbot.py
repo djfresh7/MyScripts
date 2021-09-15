627459aa2ed6af736fc2e35ff55ceab6bfebb74a
MorxTheGamer <82465903+morxthegamer@users.noreply.github.com>
2021-09-15 11:06:50 -0500

    Big Update - Redo (Fixed Some Bugs)
    New Super Update.

--- multipostredditbot.py
+++ multipostredditbot.py
@@ -1,4 +1,4 @@
-# Post items
+# Post Items
 t = "Video Name Here"
 u = "URL Here"
 
@@ -10,25 +10,24 @@ subreddits = [
 # Posting to all of the above subs
 count = 0
 for subreddit in subreddits:
-  count+=1
+  count += 1
   try:
     reddit.subreddit(subreddit).submit(title=t, url=u, send_replies=True)
     print("Successfully posted to: ", subreddit, "Posted to", count, "of", len(subreddits), "subreddits")
   except praw.exceptions.RedditAPIException as exception:
     for subexception in exception.items:
       if subexception.error_type == "RATELIMIT":
-        wait = str(subexception).replace("RATELIMIT: 'Looks like you've been doing that a lot. Take a break for ", "")
+        wait = str(subexception.error_message)
+        wait = print(wait)
         if "minute" in wait:
-          wait = wait[:70]
+          wait = wait[70:71]
           wait = int(wait) + 1
           print("Need to wait" + wait)
         else:
           wait = 1
-        
         print("Waiting for", wait, "minutes")
         time.sleep(wait*60)
         reddit.subreddit(subreddit).submit(title=t, url=u, send_replies=True)
         print("Successfully posted to", subreddit)
-        
       else:
-        break
+        continue
