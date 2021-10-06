aad8729f5a45fc08173bfcc3a702c0a433e3cc97
MorxTheGamer <82465903+morxthegamer@users.noreply.github.com>
2021-10-06 11:05:27 -0500

    A Simple JavaScript File.
    Another simple example JS script.

--- example.js
+++ example.js
@@ -5,3 +5,34 @@ const Med = "ok"
 // Prints both the variable and constant.
 console.log(no)
 console.log(Med)
+
+// Simple JS calculator.
+
+function calculator(num1, op, num2){
+  if (op == "+"){
+    return num1 + num2
+  } else if (op == "-"){
+    return num1 - num2
+  } else if (op == "*"){
+    return num1 * num2
+  } else if (op == "/"){
+    return num1 / num2
+  } else{
+    return false
+  }
+}
+
+// JS variables
+var a = 0
+let g = 2
+
+// Simple JS checks
+if (g == a){
+    console.log("Equal")
+} else if (g > a){
+    console.log("G is bigger")
+} else if (g < a){
+    console.log("A is bigger")
+} else{
+    console.log(false)
+}
